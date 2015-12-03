1a74eca0ec0bba70b51b8280c12c3d49b7b5ed23
Alexsandro Haag <alex@hgsoft.com.br>
2015-12-03 15:36:11 -0200

    Criadas ações para os botões que definem o estado.

--- src/game2dms/Console.java
+++ src/game2dms/Console.java
@@ -27,77 +27,86 @@ public class Console extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jPanel1 = new javax.swing.JPanel();
-        jPanel2 = new javax.swing.JPanel();
-        jButton1 = new javax.swing.JButton();
-        jButton2 = new javax.swing.JButton();
-        jButton3 = new javax.swing.JButton();
-        jButton4 = new javax.swing.JButton();
+        jPCenario = new javax.swing.JPanel();
+        jlEstado = new javax.swing.JLabel();
+        jPComando = new javax.swing.JPanel();
+        jBAndar = new javax.swing.JButton();
+        jBPular = new javax.swing.JButton();
+        JBMorrer = new javax.swing.JButton();
+        jBParar = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
-        jPanel1.setLayout(jPanel1Layout);
-        jPanel1Layout.setHorizontalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 0, Short.MAX_VALUE)
+        jlEstado.setText("Estado");
+
+        javax.swing.GroupLayout jPCenarioLayout = new javax.swing.GroupLayout(jPCenario);
+        jPCenario.setLayout(jPCenarioLayout);
+        jPCenarioLayout.setHorizontalGroup(
+            jPCenarioLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPCenarioLayout.createSequentialGroup()
+                .addGap(26, 26, 26)
+                .addComponent(jlEstado)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
-        jPanel1Layout.setVerticalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 382, Short.MAX_VALUE)
+        jPCenarioLayout.setVerticalGroup(
+            jPCenarioLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPCenarioLayout.createSequentialGroup()
+                .addGap(46, 46, 46)
+                .addComponent(jlEstado)
+                .addContainerGap(321, Short.MAX_VALUE))
         );
 
-        jButton1.setText("Andar");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
+        jBAndar.setText("Andar");
+        jBAndar.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
+                jBAndarActionPerformed(evt);
             }
         });
 
-        jButton2.setText("Pular");
-        jButton2.addActionListener(new java.awt.event.ActionListener() {
+        jBPular.setText("Pular");
+        jBPular.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton2ActionPerformed(evt);
+                jBPularActionPerformed(evt);
             }
         });
 
-        jButton3.setText("Morrer");
-        jButton3.addActionListener(new java.awt.event.ActionListener() {
+        JBMorrer.setText("Morrer");
+        JBMorrer.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton3ActionPerformed(evt);
+                JBMorrerActionPerformed(evt);
             }
         });
 
-        jButton4.setText("Parar");
-        jButton4.addActionListener(new java.awt.event.ActionListener() {
+        jBParar.setText("Parar");
+        jBParar.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton4ActionPerformed(evt);
+                jBPararActionPerformed(evt);
             }
         });
 
-        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
-        jPanel2.setLayout(jPanel2Layout);
-        jPanel2Layout.setHorizontalGroup(
-            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel2Layout.createSequentialGroup()
-                .addComponent(jButton1)
+        javax.swing.GroupLayout jPComandoLayout = new javax.swing.GroupLayout(jPComando);
+        jPComando.setLayout(jPComandoLayout);
+        jPComandoLayout.setHorizontalGroup(
+            jPComandoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPComandoLayout.createSequentialGroup()
+                .addComponent(jBAndar)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jButton2)
+                .addComponent(jBPular)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jButton3)
+                .addComponent(JBMorrer)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jButton4)
+                .addComponent(jBParar)
                 .addGap(0, 340, Short.MAX_VALUE))
         );
-        jPanel2Layout.setVerticalGroup(
-            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
+        jPComandoLayout.setVerticalGroup(
+            jPComandoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPComandoLayout.createSequentialGroup()
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jButton1)
-                    .addComponent(jButton2)
-                    .addComponent(jButton3)
-                    .addComponent(jButton4))
+                .addGroup(jPComandoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jBAndar)
+                    .addComponent(jBPular)
+                    .addComponent(JBMorrer)
+                    .addComponent(jBParar))
                 .addContainerGap())
         );
 
@@ -105,36 +114,36 @@ public class Console extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-            .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(jPCenario, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(jPComando, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(jPCenario, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(jPComando, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(0, 0, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jButton1ActionPerformed
+    private void jBAndarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBAndarActionPerformed
+        jlEstado.setText("Andando");
+    }//GEN-LAST:event_jBAndarActionPerformed
 
-    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jButton2ActionPerformed
+    private void jBPularActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBPularActionPerformed
+        jlEstado.setText("Pulando");
+    }//GEN-LAST:event_jBPularActionPerformed
 
-    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jButton3ActionPerformed
+    private void JBMorrerActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_JBMorrerActionPerformed
+        jlEstado.setText("Morri");
+    }//GEN-LAST:event_JBMorrerActionPerformed
 
-    private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jButton4ActionPerformed
+    private void jBPararActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBPararActionPerformed
+        jlEstado.setText("Parei");
+    }//GEN-LAST:event_jBPararActionPerformed
 
     /**
      * @param args the command line arguments
@@ -172,11 +181,12 @@ public class Console extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
-    private javax.swing.JButton jButton3;
-    private javax.swing.JButton jButton4;
-    private javax.swing.JPanel jPanel1;
-    private javax.swing.JPanel jPanel2;
+    private javax.swing.JButton JBMorrer;
+    private javax.swing.JButton jBAndar;
+    private javax.swing.JButton jBParar;
+    private javax.swing.JButton jBPular;
+    private javax.swing.JPanel jPCenario;
+    private javax.swing.JPanel jPComando;
+    private javax.swing.JLabel jlEstado;
     // End of variables declaration//GEN-END:variables
 }
